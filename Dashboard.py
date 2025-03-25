d3be14eb953a54dd260215f2e6c3cc1d721450a2
Maxray <maxoupoke2001@gmail.com>
2025-03-25 15:57:19 +0100

    API_Key and fix

--- Dashboard.py
+++ Dashboard.py
@@ -14,7 +14,7 @@ def load_data():
     return pd.read_csv("https://raw.githubusercontent.com/Quera-fr/Python-Programming/refs/heads/main/data.csv")
 
 try:
-    st.sidebar.write(st.secrets['API_KEY'])
+    st.sidebar.write(('Votre clé API est :'),(st.secrets['API_KEY']))
 except:
     st.error('il n\'y a pas de clé API')
 
@@ -36,38 +36,7 @@ if st.checkbox('Afficher le df'):
 
     st.write(df)
 
-if st.checkbox('Afficher le formulaire'):
-
-    name = st.text_input('Entrez votre nom')
-    if name != "":
-        st.write(f"Salut, {name}")
-
-with st.form(key='my_form'):
-    
-    col1, col2 = st.columns(2)
-    
-    with col1:
-        profession = st.selectbox("Selectioné une profession", df.Profession.unique())
-        
-        age = st.slider("Selectionnez un age", df.Age.min(), df.Age.max(), (df.Age.min(), df.Age.max()))
-        valid = st.form_submit_button("Valider")
-        
-    with col2:
-        data_age = df[(df['Profession'] == profession) & (df['Age'] >= age[0]) & (df['Age'] <= age[1])].Age
-
-        if valid:
-            plot = sns.histplot(data_age, bins=age[1]-age[0])
-            st.pyplot(plot.figure)
-
-st.subheader('Presentation de data')
-
-st.write('Presentation des data avec streamlit')
-
-if st.checkbox('Afficher le df'):
-
-    st.write(df)
-
-if st.checkbox('Afficher le formulaire'):
+if st.checkbox('Afficher le formulaire',key="checkbox1"):
 
     name = st.text_input('Entrez votre nom')
     if name != "":
